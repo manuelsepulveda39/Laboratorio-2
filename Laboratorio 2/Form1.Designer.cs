48f6520bc27bdf6bc2c804d125b3c144967f088d
Manuel Sepulveda <manuelsegaviria@gmail.com>
2023-09-30 14:27:38 -0500

    Primer commit

--- Laboratorio 2/Form1.Designer.cs
+++ Laboratorio 2/Form1.Designer.cs
@@ -28,20 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxRectaUno = new System.Windows.Forms.TextBox();
+            this.TextBoxRectaDos = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.erroresUno = new System.Windows.Forms.Label();
+            this.erroresDos = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Recta 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Recta 2";
+            // 
+            // TextBoxRectaUno
+            // 
+            this.TextBoxRectaUno.Location = new System.Drawing.Point(21, 116);
+            this.TextBoxRectaUno.Name = "TextBoxRectaUno";
+            this.TextBoxRectaUno.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxRectaUno.TabIndex = 2;
+            // 
+            // TextBoxRectaDos
+            // 
+            this.TextBoxRectaDos.Location = new System.Drawing.Point(201, 116);
+            this.TextBoxRectaDos.Name = "TextBoxRectaDos";
+            this.TextBoxRectaDos.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxRectaDos.TabIndex = 3;
+            // 
+            // calcular
+            // 
+            this.calcular.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.calcular.Location = new System.Drawing.Point(121, 208);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 23);
+            this.calcular.TabIndex = 4;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // erroresUno
+            // 
+            this.erroresUno.AutoSize = true;
+            this.erroresUno.Location = new System.Drawing.Point(18, 167);
+            this.erroresUno.Name = "erroresUno";
+            this.erroresUno.Size = new System.Drawing.Size(35, 13);
+            this.erroresUno.TabIndex = 5;
+            this.erroresUno.Text = "label3";
+            this.erroresUno.Visible = false;
+            // 
+            // erroresDos
+            // 
+            this.erroresDos.AutoSize = true;
+            this.erroresDos.Location = new System.Drawing.Point(198, 167);
+            this.erroresDos.Name = "erroresDos";
+            this.erroresDos.Size = new System.Drawing.Size(35, 13);
+            this.erroresDos.TabIndex = 6;
+            this.erroresDos.Text = "label3";
+            this.erroresDos.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(329, 367);
+            this.Controls.Add(this.erroresDos);
+            this.Controls.Add(this.erroresUno);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.TextBoxRectaDos);
+            this.Controls.Add(this.TextBoxRectaUno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Intercepto entre rectas";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxRectaUno;
+        private System.Windows.Forms.TextBox TextBoxRectaDos;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label erroresUno;
+        private System.Windows.Forms.Label erroresDos;
     }
 }
 
--- Laboratorio 2/Form1.cs
+++ Laboratorio 2/Form1.cs
@@ -12,9 +12,137 @@ namespace Laboratorio_2
 {
     public partial class Form1 : Form
     {
+        String error = "La formula esta mal escrita";
+        String errorLetra = "Solo se permiten numeros";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calcular_Click(object sender, EventArgs e)
+        {
+            erroresUno.Visible = false;
+            erroresDos.Visible = false;
+            String rectaUno = TextBoxRectaUno.Text;
+            String rectaDos = TextBoxRectaDos.Text;
+            rectaUno = rectaUno.Trim();
+            rectaDos = rectaDos.Trim();
+            rectaUno = rectaUno.ToLower();
+            rectaDos = rectaDos.ToLower();
+            int posicionIgualUno = rectaUno.IndexOf("=");
+            int posicionIgualDos = rectaDos.IndexOf("=");
+            int posicionXUno = rectaUno.IndexOf("x");
+            int posicionXDos = rectaDos.IndexOf("x");
+            int posicionYUno = rectaUno.IndexOf("y");
+            int posicionYDos = rectaDos.IndexOf("y");
+            
+            if ((posicionIgualUno == -1 || posicionXUno == -1 || posicionYUno == -1) && 
+                (posicionIgualDos == -1 || posicionXDos == -1 || posicionYDos == -1))
+            {
+
+            }
+            else if (posicionIgualUno == -1 || posicionXUno == -1 || posicionYUno == -1)
+            {
+                erroresUno.Text = error;
+                erroresUno.Visible = true;
+            }
+            else if (posicionIgualDos == -1 || posicionXDos == -1 || posicionYDos == -1)
+            { 
+                erroresDos.Text = error;
+                erroresDos.Visible = true;
+            }
+            else
+            {
+                if (posicionXUno < posicionIgualUno || posicionIgualUno < posicionYUno || posicionYUno != 0 || posicionIgualUno != 1)
+                {
+                    erroresUno.Text = error;
+                    erroresUno.Visible = true;
+                }
+                if (posicionXDos < posicionIgualDos || posicionIgualDos < posicionYDos || posicionYDos != 0 || posicionIgualDos != 1)
+                {
+                    erroresDos.Text = error;
+                    erroresDos.Visible = true;
+                }
+                if (posicionYUno < posicionIgualUno && posicionIgualUno < posicionXUno && posicionYDos < posicionIgualDos
+                    && posicionIgualDos < posicionXDos && posicionIgualDos == 1 && posicionIgualUno == 1 && posicionYUno == 0
+                    && posicionYDos == 0)
+                {
+                    int intervaloUno = posicionXUno - 2;
+                    String pendienteUnoString = rectaUno.Substring(2 , intervaloUno);
+                    int intervaloDos = posicionXDos - 2;
+                    String pendienteDosString = rectaDos.Substring(2, intervaloDos);
+                    float pendienteUno;
+                    float pendienteDos;
+                    if ((float.TryParse(pendienteUnoString, out  pendienteUno) || posicionXUno == posicionIgualUno + 1) &&
+                        (float.TryParse(pendienteDosString, out pendienteDos) || posicionXDos == posicionIgualDos + 1))
+                    {
+                        if (posicionXUno == posicionIgualUno + 1)
+                        {
+                            pendienteUno = 1;
+                        }
+                        if (posicionXDos == posicionIgualDos + 1)
+                        {
+                            pendienteDos = 1;
+                        }
+                        String interceptoUnoString = rectaUno.Substring(posicionXUno + 1);
+                        String interceptoDosString = rectaDos.Substring(posicionXDos + 1);
+                        float interceptoUno;
+                        float interceptoDos;
+                        if ((float.TryParse(interceptoUnoString, out interceptoUno) || interceptoUnoString.Length == posicionXUno + 1)
+                            && (float.TryParse(interceptoDosString, out interceptoDos) || interceptoDosString.Length == posicionXDos + 1 ))
+                        {
+                            if(interceptoUnoString.Length == posicionXUno + 1)
+                            {
+                                interceptoUno = 0;
+                            }
+                            if (interceptoDosString.Length == posicionXDos + 1)
+                            {
+                                interceptoDos = 0;
+                            }
+                        }
+                        else
+                        {
+                            if (float.TryParse(interceptoUnoString, out interceptoUno))
+                            {
+                                erroresDos.Text = errorLetra;
+                                erroresDos.Visible = true;
+                            }
+                            if (float.TryParse(interceptoDosString, out interceptoDos))
+                            {
+                                erroresUno.Text = errorLetra;
+                                erroresUno.Visible = true;
+                            }
+                            else if (!(float.TryParse(interceptoUnoString, out interceptoUno)))
+                            {
+                                erroresDos.Text = errorLetra;
+                                erroresDos.Visible = true;
+                                erroresUno.Text = errorLetra;
+                                erroresUno.Visible = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(float.TryParse(pendienteUnoString,out pendienteUno))
+                        {
+                            erroresDos.Text = errorLetra;
+                            erroresDos.Visible = true;
+                        }
+                        if (float.TryParse(pendienteDosString, out pendienteDos))
+                        {
+                            erroresUno.Text = errorLetra;
+                            erroresUno.Visible = true;
+                        }
+                        else if (!(float.TryParse(pendienteUnoString, out pendienteUno)))
+                        {
+                            erroresDos.Text = errorLetra;
+                            erroresDos.Visible = true;
+                            erroresUno.Text = errorLetra;
+                            erroresUno.Visible = true;
+                        }
+                    }
+                }
+            }
         }
     }
 }
