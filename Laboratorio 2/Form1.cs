2a1e522369a97cd38a867ab8f56aaaa405b904d5
Manuel Sepulveda <manuelsegaviria@gmail.com>
2023-10-05 20:59:19 -0500

    funciones y parte del main

--- Laboratorio 2/Form1.cs
+++ Laboratorio 2/Form1.cs
@@ -20,6 +20,10 @@ namespace Laboratorio_2
 
         private void calcular_Click(object sender, EventArgs e)
         {
+            float pendienteUno = 0;
+            float pendienteDos = 0;
+            float interceptoUno = 0;
+            float interceptoDos = 0;
             errores.Visible = false;
             String rectaUno = TextBoxRectaUno.Text;
             String rectaDos = TextBoxRectaDos.Text;
@@ -27,34 +31,91 @@ namespace Laboratorio_2
             rectaDos = rectaDos.Trim();
             rectaUno = rectaUno.ToLower();
             rectaDos = rectaDos.ToLower();
-            if (comboBox1.Text == "y-y1=m(x-x1")
+            if (comboBox1.Text == "y-y1=m(x-x1)")
             {
                 int posicionIgual = rectaUno.IndexOf("=");
                 int posicionX = rectaUno.IndexOf("x");
                 int posicionY = rectaUno.IndexOf("y");
                 int posicionAbrPare = rectaUno.IndexOf("(");
                 int posicionCiePare = rectaUno.IndexOf(")");
-
+                if (posicionIgual != -1 && posicionX != -1 && posicionY != -1 && posicionAbrPare == -1 && posicionCiePare == -1 && 
+                    posicionY < posicionIgual && posicionIgual < posicionAbrPare && posicionAbrPare == posicionX - 1 && posicionX < posicionCiePare)
+                {
+                    pendienteUno = pendientePunPend(rectaUno, posicionIgual, posicionAbrPare);
+                    if (pendienteUno != (float)Math.PI)
+                    {
+                        interceptoUno = interceptoPunPend(rectaUno, posicionIgual, posicionX, posicionY, posicionCiePare, pendienteUno);
+                    }
+                }
+                else
+                {
+                    errores.Text = error;
+                    errores.Visible = true;
+                }
             }
-            if (comboBox2.Text == "y-y1=m(x-x1")
+            if (comboBox2.Text == "y-y1=m(x-x1)")
             {
                 int posicionIgual = rectaDos.IndexOf("=");
                 int posicionX = rectaDos.IndexOf("x");
                 int posicionY = rectaDos.IndexOf("y");
                 int posicionAbrPare = rectaDos.IndexOf("(");
                 int posicionCiePare = rectaDos.IndexOf(")");
+                if (posicionIgual != -1 && posicionX != -1 && posicionY != -1 && posicionAbrPare == -1 && posicionCiePare == -1 &&
+                    posicionY < posicionIgual && posicionIgual < posicionAbrPare && posicionAbrPare == posicionX - 1 && posicionX < posicionCiePare)
+                {
+                    pendienteDos = pendientePunPend(rectaDos, posicionIgual, posicionAbrPare);
+                    if (pendienteDos != (float)Math.PI)
+                    {
+                        interceptoDos = interceptoPunPend(rectaDos, posicionIgual, posicionX, posicionY, posicionCiePare, pendienteDos);
+                    }
+                }
+                else
+                {
+                    errores.Text = error;
+                    errores.Visible = true;
+                }
             }
             if (comboBox1.Text == "y=mx+b")
             {
                 int posicionIgual = rectaUno.IndexOf("=");
-                int posicionParen = rectaUno.IndexOf("Y");
+                int posicionY = rectaUno.IndexOf("Y");
                 int posicionX = rectaUno.IndexOf("x");
+                if (posicionIgual != -1 && posicionY != -1 && posicionX != -1 && posicionY == posicionIgual -1 && posicionX > posicionIgual)
+                {
+                    pendienteUno = pendientePenInte(rectaUno, posicionX, posicionIgual);
+                    if (pendienteUno != (float)Math.PI)
+                    {
+                        interceptoUno = interceptoPenInte(rectaUno, posicionX);
+                    }
+                }
+                else
+                {
+                    errores.Text = error;
+                    errores.Visible = true;
+                }
             }
             if(comboBox2.Text == "y=mx+b")
             {
                 int posicionIgual = rectaDos.IndexOf("=");
-                int posicionParen = rectaDos.IndexOf("Y");
+                int posicionY = rectaDos.IndexOf("y");
                 int posicionX = rectaDos.IndexOf("x");
+                if (posicionIgual != -1 && posicionY != -1 && posicionX != -1 && posicionY == posicionIgual - 1 && posicionX > posicionIgual)
+                {
+                    pendienteDos = pendientePenInte(rectaDos, posicionX, posicionIgual);
+                    if (pendienteDos != (float)Math.PI)
+                    {
+                        interceptoDos = interceptoPenInte(rectaDos, posicionX);
+                    }
+                }
+                else
+                {
+                    errores.Text = error;
+                    errores.Visible = true;
+                }
+            }
+            if (pendienteUno != (float)Math.PI && pendienteDos != (float)Math.PI && interceptoUno != (float)Math.PI && interceptoDos != (float)Math.PI)
+            {
+
             }
         }
 
