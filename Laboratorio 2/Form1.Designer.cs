6a0979b55185dadcaa83e08745a90faa3201c6a4
Manuel Sepulveda <manuelsegaviria@gmail.com>
2023-10-05 19:54:58 -0500

    primer commit

--- Laboratorio 2/Form1.Designer.cs
+++ Laboratorio 2/Form1.Designer.cs
@@ -33,14 +33,15 @@
             this.TextBoxRectaUno = new System.Windows.Forms.TextBox();
             this.TextBoxRectaDos = new System.Windows.Forms.TextBox();
             this.calcular = new System.Windows.Forms.Button();
-            this.erroresUno = new System.Windows.Forms.Label();
-            this.erroresDos = new System.Windows.Forms.Label();
+            this.errores = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 100);
+            this.label1.Location = new System.Drawing.Point(45, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 0;
@@ -49,7 +50,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 100);
+            this.label2.Location = new System.Drawing.Point(185, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 1;
@@ -57,14 +58,14 @@
             // 
             // TextBoxRectaUno
             // 
-            this.TextBoxRectaUno.Location = new System.Drawing.Point(21, 116);
+            this.TextBoxRectaUno.Location = new System.Drawing.Point(18, 86);
             this.TextBoxRectaUno.Name = "TextBoxRectaUno";
             this.TextBoxRectaUno.Size = new System.Drawing.Size(100, 20);
             this.TextBoxRectaUno.TabIndex = 2;
             // 
             // TextBoxRectaDos
             // 
-            this.TextBoxRectaDos.Location = new System.Drawing.Point(201, 116);
+            this.TextBoxRectaDos.Location = new System.Drawing.Point(154, 86);
             this.TextBoxRectaDos.Name = "TextBoxRectaDos";
             this.TextBoxRectaDos.Size = new System.Drawing.Size(100, 20);
             this.TextBoxRectaDos.TabIndex = 3;
@@ -72,7 +73,7 @@
             // calcular
             // 
             this.calcular.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.calcular.Location = new System.Drawing.Point(121, 208);
+            this.calcular.Location = new System.Drawing.Point(92, 147);
             this.calcular.Name = "calcular";
             this.calcular.Size = new System.Drawing.Size(75, 23);
             this.calcular.TabIndex = 4;
@@ -80,34 +81,49 @@
             this.calcular.UseVisualStyleBackColor = true;
             this.calcular.Click += new System.EventHandler(this.calcular_Click);
             // 
-            // erroresUno
+            // errores
             // 
-            this.erroresUno.AutoSize = true;
-            this.erroresUno.Location = new System.Drawing.Point(18, 167);
-            this.erroresUno.Name = "erroresUno";
-            this.erroresUno.Size = new System.Drawing.Size(35, 13);
-            this.erroresUno.TabIndex = 5;
-            this.erroresUno.Text = "label3";
-            this.erroresUno.Visible = false;
+            this.errores.AutoSize = true;
+            this.errores.Location = new System.Drawing.Point(113, 120);
+            this.errores.Name = "errores";
+            this.errores.Size = new System.Drawing.Size(35, 13);
+            this.errores.TabIndex = 5;
+            this.errores.Text = "label3";
+            this.errores.Visible = false;
             // 
-            // erroresDos
+            // comboBox1
             // 
-            this.erroresDos.AutoSize = true;
-            this.erroresDos.Location = new System.Drawing.Point(198, 167);
-            this.erroresDos.Name = "erroresDos";
-            this.erroresDos.Size = new System.Drawing.Size(35, 13);
-            this.erroresDos.TabIndex = 6;
-            this.erroresDos.Text = "label3";
-            this.erroresDos.Visible = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y-y1=m(x-x1)",
+            "y=mx+b"});
+            this.comboBox1.Location = new System.Drawing.Point(26, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(86, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "y=mx+b";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "y-y1=m(x-x1)",
+            "y=mx+b"});
+            this.comboBox2.Location = new System.Drawing.Point(162, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(86, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "y=mx+b";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.ClientSize = new System.Drawing.Size(329, 367);
-            this.Controls.Add(this.erroresDos);
-            this.Controls.Add(this.erroresUno);
+            this.ClientSize = new System.Drawing.Size(274, 367);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.errores);
             this.Controls.Add(this.calcular);
             this.Controls.Add(this.TextBoxRectaDos);
             this.Controls.Add(this.TextBoxRectaUno);
@@ -128,8 +144,9 @@
         private System.Windows.Forms.TextBox TextBoxRectaUno;
         private System.Windows.Forms.TextBox TextBoxRectaDos;
         private System.Windows.Forms.Button calcular;
-        private System.Windows.Forms.Label erroresUno;
-        private System.Windows.Forms.Label erroresDos;
+        private System.Windows.Forms.Label errores;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
